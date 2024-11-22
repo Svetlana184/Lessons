9aba520e6435bc79451bf6467d86f0183a017574
Svetlana184 <sveta.kgd.39@gmail.com>
2024-11-22 16:26:55 +0200

    н

--- lesson18/Program.cs
+++ lesson18/Program.cs
@@ -1,7 +1,7 @@
 ﻿// ФУНКЦИИ
 
 string lang = "en";
-switch(lang)
+switch (lang)
 {
     case "en": SayHello(); break;
     case "ru": SayHelloRu(); break;
@@ -14,10 +14,13 @@ Console.WriteLine("введите x");
 int x = int.Parse(Console.ReadLine());
 Console.WriteLine("введите y");
 int y = int.Parse(Console.ReadLine());
-Sum(x,y);
-
-
-
+Sum(x, y);
+Console.WriteLine("введите ваше имя");
+string name = Console.ReadLine();
+Console.WriteLine("введите ваш возраст");
+int age = int.Parse(Console.ReadLine());
+PrintPerson(name, age, "Голубчиков энтертэймент");
+PrintPerson(age: 30, name: "B", company: "A");
 
 void SayHello()
 {
@@ -29,5 +32,63 @@ void SayHelloRu()
 }
 void SayHelloDe() => Console.WriteLine("pepe!"); //лямбда-выражение
 void WriteMyName(string name) => Console.WriteLine("ваше имя - " + name);
-void Sum(int a, int b) => Console.WriteLine($"{a}+{b}={a+b}");
-void PrintPerson(string name, int age) => Console.WriteLine($"Меня зовут {name}, мне {age}");
+void Sum(int a, int b) => Console.WriteLine($"{a}+{b}={a + b}");
+void PrintPerson(string name, int age, string company = "samsung") => Console.WriteLine($"Меня зовут {name}, мне {age}, я работаю в {company}");
+
+void PrintLine()
+{
+    for (int i = 0; i < 80; i++) Console.Write("*");
+}
+
+PrintLine();
+Console.WriteLine();
+//возвращение значений и оператор return
+void PrintPerson(string name, int age, string company = "samsung")
+{
+    if (age < 0 || age > 120)
+    {
+        Console.WriteLine("такой возраст не существует");
+        return;
+    }
+    Console.WriteLine($"Меня зовут {name}, мне {age}, я работаю в {company}");
+}
+PrintPerson("i", 1);
+int Summa(int a, int b)
+{
+    return a + b;
+}
+int Min(int a, int b) => a - b;
+int Sub(int a, int b) => a + b;
+int Mult(int a, int b) => a * b;
+int Div(int a, int b)
+{
+    if (b == 0) return a / b;
+    else return 0;
+}
+
+Console.WriteLine("введите х");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("введите y");
+int y = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите операцию(+, -, *, /)");
+char op = char.Parse(Console.ReadLine());
+switch (op)
+{
+    case '+': Console.WriteLine($"{x}+{y}={Summa(x, y)}"); break;
+    case '-': Console.WriteLine($"{x}-{y}={Min(x, y)}"); break;
+    case '*': Console.WriteLine($"{x}*{y}={Mult(x, y)}"); break;
+    case '/': Console.WriteLine($"{x}/{y}={Div(x, y)}"); break;
+    default:
+        Console.WriteLine("Такой операции нет");
+        break;
+}
+
+double Func(int x)
+{
+    return (Math.Sqrt(x) + x) / 2;
+}
+double y = Func(6) + Func(13) + Func(21);
+Console.WriteLine($"y = {y:F2}");
+
+
+
---
+++ lesson19/Program.cs
@@ -0,0 +1,90 @@
+﻿//ПЕРЕДАЧА ПАРАМЕТРОВ В ФУНКЦИЮ
+
+void Swap(ref int x, ref int y) //передача параметра по ссылке
+{
+    int temp = x; 
+    x = y;
+    y = temp;
+}
+int a = 6;
+int b = 7;
+Console.WriteLine($"a = {a}, b = {b}");
+Swap(ref a, ref b);
+Console.WriteLine($"a = {a}, b = {b}"); //a = 7, b = 6
+
+void Swap1(int x,int y) //передача параметра по значению
+{
+    int temp = x;
+    x = y;
+    y = temp;
+}
+a = 6;
+b = 7;
+Console.WriteLine($"a = {a}, b = {b}");
+Swap1(a, b);
+Console.WriteLine($"a = {a}, b = {b}"); //a = 6, b = 7
+
+//выходные параметры. Модификатор out
+
+int Summa(int x,int y) => x + y;
+
+
+Console.WriteLine(Summa(a,b));
+
+void SummaOut(int x, int y, out int res)
+{
+    res = a + b;
+}
+int res;
+SummaOut(a,b,out res);
+Console.WriteLine("res = "+res);
+SummaOut(a,b,out int rez);
+Console.WriteLine("rez = "+rez);
+
+//входные параметры. Модификатор in
+
+int Square(in int n) //теперь изменять n внутри функции нельзя
+{
+    //n = 8;
+    return n * n;
+}
+
+
+Console.WriteLine("5 * 5 = " + Square(5));
+
+//массив параметров и ключевое слово params
+
+int SumParams(params int[] numbers)
+{
+    int sum = 0;
+    foreach (int n in numbers)
+    {
+        sum += n;
+    }
+    return sum;
+}
+Console.WriteLine(SumParams(5, 6, 7, 0, 1, 2));
+
+int SumMas(int[] mas)
+{
+    int sum = 0;
+    foreach (int n in mas)
+    {
+        sum += n;
+    }
+    return sum;
+}
+Random rand = new Random();
+int[] massive = new int[10];
+for (int i = 0; i < massive.Length; i++)
+{
+    massive[i] = rand.Next(1, 3);
+    Console.Write(massive[i] + " ");
+}
+Console.WriteLine();
+Console.WriteLine(SumMas(massive));
+
+
+//Рекурсивные функции
+//рекурсивная фнкция - такая конструкция, при которой функция вызывает саму себя
+
