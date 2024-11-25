f75f0a5e6aadc4c953f906d870fdefe7ea27174f
Svetlana184 <sveta.kgd.39@gmail.com>
2024-11-25 14:52:00 +0200

    е

---
+++ ClassLibr_6.2/Class1.cs
@@ -0,0 +1,39 @@
+﻿namespace ClassLibr_6_2
+{
+    public class Class6_2
+    {
+        public static double[] MinSt(double[,] mas)
+        {
+            double minSum = double.MaxValue;
+            int stIndex = -1;
+            for (int i = 0; i < mas.GetLength(1); i++)
+            {
+                double sumSt = 0;
+                for (int j = 0; j < mas.GetLength(0); j++)
+                {
+                    sumSt += Math.Abs(mas[j,i]);
+                }
+                if (sumSt < minSum)
+                {
+                    minSum = sumSt;
+                    stIndex = i;
+                }
+            }
+            double[] vector = new double[3];
+            for (int i = 0;i < mas.GetLength(0);i++)
+            {
+                vector[i] = mas[i,stIndex];
+            }
+            return vector;
+        }
+        public static double Rez(double[] mas)
+        {
+            double proizv = 1;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (i % 2 == 0) proizv *= mas[i];
+            }
+            return proizv;
+        }
+    }
+}
---
+++ LAB_6.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using static ClassLibr_6_2.Class6_2;
+
+
+double[,] masMain = new double[3,3];
+for (int i = 0; i < masMain.GetLength(0); i++)
+{
+    for (int j = 0; j < masMain.GetLength(1); j++)
+    {
+        masMain[i, j] = Math.Pow(Math.Log(2 * (i+1) + 8 * (j+1)), 8) - Math.Pow(Math.E, Math.E); 
+        Console.Write($"{masMain[i,j]:F2} ");
+    }
+    Console.WriteLine();
+}
+double[] vector = MinSt(masMain);
+Console.WriteLine("\nстолбец с минимальной суммой модулей элементов: ");
+foreach (double v in vector) Console.WriteLine($"{v:F2} ");
+double rez = Rez(vector);
+Console.WriteLine($"\nпроизведение элементов столбца с чётными индексами: {rez:F2}");
